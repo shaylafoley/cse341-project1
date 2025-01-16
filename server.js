cf5f99f06760f747f0aa814323908156b3c8c7c1
shaylafoley <wholemotherhood@gmail.com>
2025-01-16 17:04:51 -0500

    post put delete

--- server.js
+++ server.js
@@ -21,16 +21,15 @@
 
 
  const express = require('express');
-
+const bodyParser = require('body-parser');
  const mongodb = require('./data/database');
  const app = express();
 
  const port = process.env.PORT || 5173;
 
+ app.use(bodyParser.json());
  app.use('/', require('./routes'));
-
-
-
+ 
  mongodb.initDb((err) => {
      if(err) {
          console.log(err);
